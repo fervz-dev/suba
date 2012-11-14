3788c798597291a4bf45b93fd2de60b7c244013a
RichardCa <ricardo.cristobal.a@gmail.com>
2012-11-14 12:12:13 -0600

    First commit

--- application/views/principal.php
+++
@@ -1,9 +0,0 @@
-<?php $this->load->view('header');?>
-<!-- menu superior -->
-<?php $this->load->view('menus'); ?>
-<!-- menu lateral izquierdo -->
-<?php $this->load->view('menu_lateral'); ?>
-<!-- contenido  -->
-
-<!-- footer -->
-<?php $this->load->view('footer'); ?>
